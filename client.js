c859928150933855133db123c61c2cc68bff699d
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-02-16 15:40:05 -0800

    set up client side js

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // let timer = 500;
+  // const move = (str) => {
+  //   setTimeout(() => {
+  //     conn.write(str), timer;
+  //     timer += 500;
+  //   });
+  // };
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+  conn.on("connect", () => {
+    conn.write("Name: DZ");
+    // conn.write("Move: up");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: right");
+  });
+
+  return conn;
+};
+
+module.exports = connect;
---
+++ play.js
@@ -0,0 +1,41 @@
+const net = require("net");
+const connect = require("./client");
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+};
+
+// establishes a connection with the game server
+
+console.log("Connecting ...");
+
+setupInput(connect());
+
+// setup interface to handle user input from stdin
